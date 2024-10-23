9d25f191141abd790d0a6ca9eb808594bd468f8f
nallujin <sako75hz@gmail.com>
2024-10-23 20:06:09 +0700

    Finish the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const formula = ((num-32)*(5/9));
+  const rawAnswer = Number.isInteger(formula)
+  if (rawAnswer) {
+    return formula;
+  } else {
+    return Number(formula.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const formula = (num * (9 / 5) + 32);
+  const rawAnswer = Number.isInteger(formula);
+  if (rawAnswer) {
+    return formula;
+  } else {
+    return Number(formula.toFixed(1));
+  }
 };
 
 // Do not edit below this line
