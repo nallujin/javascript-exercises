bcf545e42b6e0fcba0f5e74b671f602840662492
nallujin <sako75hz@gmail.com>
2024-10-23 19:01:43 +0700

    Finish the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, finalNum) {
+    const isValid = (Number.isInteger(startNum) && startNum > 0)
+        && (Number.isInteger(finalNum) && finalNum > 0);
+    let finalOutput = 0;
+    if (isValid) {
+        if (startNum > finalNum) {
+            [startNum, finalNum] = [finalNum, startNum];
+        }
+        for (let i = startNum; i <= finalNum; i++) {
+            finalOutput += i;
+        }
+    } else {
+        return "ERROR";
+    }
+    return finalOutput;
 };
 
 // Do not edit below this line
