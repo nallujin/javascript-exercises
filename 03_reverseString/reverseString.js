e2f8a0d4cad2e43542c5027b58d12e09c42949f5
nallujin <sako75hz@gmail.com>
2024-10-23 12:30:04 +0700

    Add the functionality

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strLastEle = str.length-1;
+    let newStr = "";
+    for (let i = strLastEle; i >= 0; i--) {
+        newStr += str.at(i);
+    }
+    return newStr;
 };
 
 // Do not edit below this line
