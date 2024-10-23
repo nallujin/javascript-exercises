70a3042146f8e655ec48fbddf4b7863d5a23088c
nallujin <sako75hz@gmail.com>
2024-10-23 12:12:44 +0700

    Add the functionality

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (str === "" || num === 0) {
+        return "";
+    } else if (str !== "" && num >= 1) {
+        let theWord = str;
+        for (let i = 0; i < num-1; i++) {
+            theWord += str;
+        }
+        return theWord;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
