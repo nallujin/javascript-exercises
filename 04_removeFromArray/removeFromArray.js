dbbe7ebcd83dd88ce93c359bd2197fd17018563a
nallujin <sako75hz@gmail.com>
2024-10-23 14:23:54 +0700

    Finish the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+    const newArr = arr;
+    numbers.forEach((num) => {
+        let index = newArr.indexOf(num);
+            while (index !== -1) {
+                newArr.splice(index, 1);
+                index = newArr.indexOf(num); // Find the next occurrence of 2
+            }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
